d28ce7be5305f5e0b31985858fe0633be3ec1cd5
biteworks <twitter@tobiaswilhelm.info>
2020-12-21 09:50:04 +0100

    Changed Icon and removed Copyright

--- CopyFolderTool/Properties/AssemblyInfo.cs
+++ CopyFolderTool/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("CopyFolderTool")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple Robocopy GUI")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tobias Wilhelm")]
 [assembly: AssemblyProduct("CopyFolderTool")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
